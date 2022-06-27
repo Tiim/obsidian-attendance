5d0e2c2222eec50583a7948ddebf11ab92440df1
Tiim <tim.bachmann96@gmail.com>
2022-06-27 22:54:54 +0200

    fix: refresh summaries when pressing the refresh button

--- src/ui/view/AttendanceOverview.svelte
+++ src/ui/view/AttendanceOverview.svelte
@@ -1,82 +1,81 @@
 <script lang="ts">
-  import { onMount } from "svelte";
-	import type { Attendance, AttendanceCodeblock } from "src/AttendanceData";
-	import Footer from "./Footer.svelte";
-	import ListView from "./ListView.svelte";
-	import Searchbar from "./Searchbar.svelte";
-	import { filterCodeblocks, type Search } from "../../util/filter-codeblocks";
-	import { exportAttendance } from "../../util/export";
-	import { setIcon, TFile, type App } from "obsidian";
-	import type AttendancePlugin from "../../main";
-import type { QueryResolver } from "src/resolver/query-resolver";
+  import type { Attendance, AttendanceCodeblock } from "src/AttendanceData";
+  import Footer from "./Footer.svelte";
+  import ListView from "./ListView.svelte";
+  import Searchbar from "./Searchbar.svelte";
+  import { filterCodeblocks, type Search } from "../../util/filter-codeblocks";
+  import { exportAttendance } from "../../util/export";
+  import { TFile, type App } from "obsidian";
+  import type AttendancePlugin from "../../main";
+  import type { QueryResolver } from "src/resolver/query-resolver";
 
-	export let plugin: AttendancePlugin;
-	let resolver = plugin.queryResolver;
-	export let app: App;
+  export let plugin: AttendancePlugin;
+  let resolver = plugin.queryResolver;
+  export let app: App;
   export let queryResolver: QueryResolver;
-	
+
   let codeblocks: AttendanceCodeblock[] = [];
-	let search: Search = {};
+  let search: Search = {};
   let listview: ListView;
 
   async function update() {
     codeblocks = [...(await queryResolver.getCodeblocks())];
-    listview.refreshSummaries();
+    setTimeout(() => listview.refreshSummaries(), 400);
   }
 
-	async function onExport() {
-		const cb = filterCodeblocks(codeblocks, search);
-		await exportAttendance(cb, plugin);
-	}
+  async function onExport() {
+    const cb = filterCodeblocks(codeblocks, search);
+    await exportAttendance(cb, plugin);
+  }
 
-	function open(a: CustomEvent<Attendance>) {
-		const tFile = app.vault.getAbstractFileByPath(a.detail.path);
-		if (tFile instanceof TFile) {
-			app.workspace.getUnpinnedLeaf().openFile(tFile, {active: true});
-		}
-	}
+  function open(a: CustomEvent<Attendance>) {
+    const tFile = app.vault.getAbstractFileByPath(a.detail.path);
+    if (tFile instanceof TFile) {
+      app.workspace.getUnpinnedLeaf().openFile(tFile, { active: true });
+    }
+  }
 </script>
 
 <div class="attendance-overview">
-	<Searchbar {app} on:export={onExport} bind:search />
-	<span class="search-string">
-		{#if search.from}
-			from {search.from.format("YYYY-MM-DD")}
-		{/if}
-		{#if search.to}
-			to {search.to.format("YYYY-MM-DD")}
-		{/if}
-	</span>
+  <Searchbar {app} on:export={onExport} bind:search />
+  <span class="search-string">
+    {#if search.from}
+      from {search.from.format("YYYY-MM-DD")}
+    {/if}
+    {#if search.to}
+      to {search.to.format("YYYY-MM-DD")}
+    {/if}
+  </span>
   <div>
     <button on:click={update}>Refresh</button>
   </div>
-	<div class="content">
-		<ListView
-    bind:this={listview}
-		on:openFile={open}
-			{resolver}
-			attendance={filterCodeblocks(codeblocks, search)}
-		/>
-	</div>
-	<Footer />
+  <div class="content">
+    <ListView
+      bind:this={listview}
+      on:openFile={open}
+      {resolver}
+      attendance={filterCodeblocks(codeblocks, search)}
+    />
+  </div>
+  <Footer />
 </div>
 
 <style>
-	.attendance-overview {
-		display: flex;
-		flex-direction: column;
-		height: 100%;
-	}
-	.content {
-		flex-grow: 1;
-		flex-shrink: 1;
-		overflow-y: auto;
-	}
-	.search-string {
-		color: var(--text-faint);
-		display: block;
-		width: 100%;
-		text-align: center;
-		margin-bottom: 10px;
-	}
+  .attendance-overview {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+  }
+  .content {
+    flex-grow: 1;
+    flex-shrink: 1;
+    overflow-y: auto;
+  }
+  .search-string {
+    color: var(--text-faint);
+    display: block;
+    width: 100%;
+    text-align: center;
+    margin-bottom: 10px;
+  }
 </style>
